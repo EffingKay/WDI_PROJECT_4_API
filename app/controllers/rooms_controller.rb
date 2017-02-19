f524e7a79c551cebd640aa65a9b965f72059e6ee
EffingKay <rozgonyiova.k@gmail.com>
2017-02-19 20:50:47 +0000

    room serializer added plus minor fixes

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,27 @@
 class RoomsController < ApplicationController
+
+  def index
+    @rooms = Room.all
+    render json: @rooms
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    render json: @room
+  end
+
+  def create
+    room = Room.new(room_params)
+    if room.save
+      render json: room, status: :ok
+    end
+  end
+
+
+  private
+    def room_params
+      hash = {}
+      hash.merge! params.slice(:title)
+      hash
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'rooms', to: 'rooms#index'
+  get 'rooms/:id', to: 'rooms#show'
+  post 'rooms', to: 'rooms#create'
+
   get 'users', to: 'users#index'
   get 'users/:id', to: 'users#show'
   post 'register', to: 'authentications#register'
