abb772dcabaee03bd43820a1b4f93ea518194023
EffingKay <rozgonyiova.k@gmail.com>
2017-02-19 20:22:34 +0000

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users', to: 'users#index'
+  get 'users/:id', to: 'users#show'
   post 'register', to: 'authentications#register'
   post 'login', to: 'authentications#login'
 end
