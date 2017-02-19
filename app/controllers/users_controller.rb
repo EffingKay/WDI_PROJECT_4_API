08581080ecdaaa38a4b646589a99037c03777061
EffingKay <rozgonyiova.k@gmail.com>
2017-02-19 14:13:54 +0000

    users index added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    # render json: { user: @user }, status: :ok
+    render json: @user
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users', to: 'users#index'
+  get 'users/:id', to: 'users#show'
+  # resources :users
   post 'register', to: 'authentications#register'
   post 'login', to: 'authentications#login'
 end
